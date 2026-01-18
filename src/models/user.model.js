43c280d9718bf0fbf012183216ee2579006de9cb
Robert <robertcjohnson1984@gmail.com>
2026-01-17 22:21:34 -0700

    refactor(model): update users schema fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,11 @@
-import { pgTable, varchar, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('users', {
-  id: varchar('id').primaryKey(),
-  name: varchar('name', {length: 255}).notNull(),
-  email: varchar('email', {length: 255}).notNull().unique(),
-  password: varchar('password', {length: 255}).notNull(),
-  role: varchar('role', {length: 50}).notNull().default('user'),
-  createdAt: timestamp().defaultNow().notNull(),
-  updatedAt: timestamp().defaultNow().notNull(),
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  password: varchar('password', { length: 255 }).notNull(),
+  role: varchar('role', { length: 50 }).notNull().default('user'),
+  created_at: timestamp().defaultNow().notNull(),
+  updated_at: timestamp().defaultNow().notNull(),
 });
-
-export default user;
